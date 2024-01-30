c85ff02672adb5348f1b386ccc117d9ce81ff946
omarlaneor <annielaneor@gmail.com>
2024-01-30 15:05:42 -0500

    Main Container done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from 'react'
- 
-function App() {
- 
+import React from "react";
 
+function App() {
   return (
-    <>
- 
-    </>
-  )
+    <div className="main-container">
+      <div className="hero-section relative">
+        <img
+          src="/assets/hero-image-wr.jpg"
+          alt="Hero Image"
+          className="w-full h-auto"
+        />
+        <div className="logo-overlay absolute mt-32 top-0 left-1/2 transform -translate-x-1/2 text-center">
+          <img
+            src="/public/assets/Logo.svg"
+            alt="Logo Img"
+            className="mx-auto mt-4"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
