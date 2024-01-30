6008966a6dcbfb22818ccec0c5f3d9da4796119e
omarlaneor <annielaneor@gmail.com>
2024-01-30 15:15:34 -0500

    Logo redimensionado

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,11 @@ function App() {
           alt="Hero Image"
           className="w-full h-auto"
         />
-        <div className="logo-overlay absolute mt-32 top-0 left-1/2 transform -translate-x-1/2 text-center">
+        <div className="text-center absolute mt-32 logo-overlay top-0 left-1/2 transform -translate-x-1/2 ">
           <img
             src="/public/assets/Logo.svg"
             alt="Logo Img"
-            className="mx-auto mt-4"
+            className="mx-auto mt-4 w-[14rem] h-auto"
           />
         </div>
       </div>
