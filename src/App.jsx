22fccbd703166e3766f3e0c331d326b2d4e61166
omarlaneor <annielaneor@gmail.com>
2024-01-31 11:24:11 -0500

    Component HeroImage Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import React from "react";
+import HeroImage from "./HeroImage/HeroImage";
 
 function App() {
   return (
     <div className="main-container">
-      <div className="hero-section relative">
-        <img
-          src="/assets/hero-image-wr.jpg"
-          alt="Hero Image"
-          className="w-full h-auto"
-        />
-        <div className="text-center absolute mt-32 logo-overlay top-0 left-1/2 transform -translate-x-1/2 ">
-          <img
-            src="/public/assets/Logo.svg"
-            alt="Logo Img"
-            className="mx-auto mt-4 w-[14rem] h-auto"
-          />
-        </div>
-      </div>
+      <HeroImage />
     </div>
   );
 }
